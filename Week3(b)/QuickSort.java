3315d1b314857d49dd1029cdc624724d2398e1eb
Mridul Singh <msmridulsingh@gmail.com>
2019-07-18 19:27:38 +0530

    Create QuickSort.java

---
+++ Week3(b)/QuickSort.java
@@ -0,0 +1,31 @@
+public class Quick{
+    private static int partition(Comparable[] a, int lo, int hi){
+        int i = lo, j = hi + 1;
+        while(true){
+            while(less(a[++i], a[lo]))
+                if(i == hi)
+                    break;
+            while(less(a[lo], a[--j]))
+                if(j == lo)
+                    break;
+            if(i > j)
+                break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+    
+    public static void sort(Comparable[] a){
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length-1);
+    }
+    
+    private static void sort(Comparable[] a, int lo, int hi){
+        if(hi <= lo)
+            return;
+        int pivot = partition(a, lo, hi);
+        sort(a, 0, pivot-1);
+        sort(a, pivot+1, hi);
+    }
+}
